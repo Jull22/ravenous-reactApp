b7e4b2b196feb0a800a895a2fb03f6963a533213
Julia Chmaj <jululuu22@gmail.com>
2021-04-21 22:18:39 +0200

    added handle on click (searchBar) and class active

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,25 +2,62 @@ import React from 'react';
 import './SearchBar.css';
 
 
-const sortByOptions = {
-    "Best Match":'best_match',
-    "Highest Rated": 'rating',
-    "Most Reviewed": 'review_count' 
-}
+
 
 class SearchBar extends React.Component{
-    renderSortByOptions(){
-         return Object.keys(sortByOptions).map((sortByOption) => {
-             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
-        });
+    constructor(props) {
+        super(props);
+
+        this.state = {term: "", location: "", sortBy: "best_match"};
+
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        
+        this.sortByOptions = {
+            "Best Match":'best_match',
+            "Highest Rated": 'rating',
+            "Most Reviewed": 'review_count' 
+        };
     }
+
+    getSortByClass(sortByOption){
+        if (sortByOption === this.state.sortBy) {
+            return 'active';
+        } else {
+            return '';
+        }
+
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({sortBy: sortByOption})
+    }
+
+    handleTermChange(e) {
+        this.setState({term: e.target.value});
+    }
+
+    handleLocationChange(e) {
+        this.setState({location: e.target.value});
+    }
+
+    renderSortByOptions(){
+         return Object.keys(this.sortByOptions).map((sortByOption) => (
+            <li key={this.sortByOptions[sortByOption]} className={this.getSortByClass(this.sortByOptions[sortByOption])}
+             onClick={this.handleSortByChange.bind(this, this.sortByOptions[sortByOption])}>{sortByOption}</li>
+            )
+        )
+    }
+    
+
+    
+
     render() {
         return (
         <div className="SearchBar">
             <div className="SearchBar-sort-options">
                 <ul>
-                    {this.renderSortByOptions}
+                    {this.renderSortByOptions()}
                 </ul>
             </div>
             <div className="SearchBar-fields">
